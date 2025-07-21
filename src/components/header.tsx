61d1f4f1ffdc069f0e0c3c1116d55c0c5bf1290f
LeonardoLobas <leonardolobas2703@gmail.com>
2025-07-21 11:01:45 -0300

    Criação Footer e Header

---
+++ src/components/header.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+import React from "react";
+import styles from "./header.module.css";
+import Image from "next/image";
+
+export default async function Header() {
+    return (
+        <header className={styles.header}>
+            <nav className={`${styles.nav} container`}>
+                <Link className={styles.logo} href={"/"}>
+                    <Image src={"/assets/dogs.svg"} alt="Dogs" width={28} height={22} priority />
+                </Link>
+                <Link className={styles.login} href={"/Login"}>
+                    Login / Criar
+                </Link>
+            </nav>
+        </header>
+    );
+}
